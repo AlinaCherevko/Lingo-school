24eb2c08845db2d21dee206d265f97d24e05de47
Alina Cherevko <alinapavluhina@ukr.net>
2024-06-24 20:13:59 +0300

    add pagination

--- src/pages/TeachersPage/TeachersPage.jsx
+++ src/pages/TeachersPage/TeachersPage.jsx
@@ -1,37 +1,49 @@
-import { useState } from 'react';
-
+import { useEffect, useState } from 'react';
 import TeachersList from '../../components/TeachersList/TeachersList';
 import { useGetAllTeachersQuery } from '../../redux/servises';
 import LanguageSelector from '../../components/Selector/LanguageSelector/LanguageSelector';
-import css from './TeachersPage.module.css';
 import CostSelector from '../../components/Selector/CostSelector/CostSelector';
 import LevelSelector from '../../components/Selector/LevelSelector/LevelSelector';
+import css from './TeachersPage.module.css';
 
 const TeachersPage = () => {
-  const { data } = useGetAllTeachersQuery();
-  const [dataToShow, setDataToShow] = useState(data);
+  const { data, isLoading } = useGetAllTeachersQuery();
+  const [dataToShow, setDataToShow] = useState([]);
+  const [visibleItemsCount, setVisibleItemsCount] = useState(4);
+  const itemsPerPage = 4;
+  console.log(visibleItemsCount);
+
+  useEffect(() => {
+    if (data) {
+      setDataToShow(data.slice(0, visibleItemsCount));
+    }
+  }, [data]);
 
   const filterByPrice = (value) => {
-    const filteredAdverts = data.filter(
+    const filteredAdverts = dataToShow.filter(
       (item) => item.price_per_hour === +value
     );
-    setDataToShow(filteredAdverts);
+    setDataToShow(filteredAdverts.slice(0, visibleItemsCount));
   };
 
   const filterByLanguage = (lang) => {
-    const filteredAdvertsByLanguage = data.filter((item) =>
+    const filteredAdvertsByLanguage = dataToShow.filter((item) =>
       item.languages.includes(lang)
     );
-    setDataToShow(filteredAdvertsByLanguage);
+    setDataToShow(filteredAdvertsByLanguage.slice(0, visibleItemsCount));
   };
 
   const filterByLevel = (level) => {
-    const filteredAdvertsByLevel = data.filter((item) =>
+    const filteredAdvertsByLevel = dataToShow.filter((item) =>
       item.levels.includes(level)
     );
-    setDataToShow(filteredAdvertsByLevel);
+    setDataToShow(filteredAdvertsByLevel.slice(0, visibleItemsCount));
   };
 
+  const onloadMore = () => {
+    setVisibleItemsCount((prevCount) => prevCount + itemsPerPage);
+    console.log(visibleItemsCount);
+  };
   return (
     <section className={css.teachersSection}>
       <div className="container">
@@ -43,6 +55,11 @@ const TeachersPage = () => {
               <CostSelector filterByPrice={filterByPrice} />
             </div>
             <TeachersList data={dataToShow} />
+            <button className={css.button} type="button" onClick={onloadMore}>
+              Load more
+            </button>
+            {dataToShow.length > 0 ||
+              (!isLoading && <p>No any matches for your request</p>)}
           </>
         )}
       </div>
